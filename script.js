38a82ba9d2f5b34b7afbcde9580e9f8351ffec94
Sava <savab2164@gmail.com>
2022-08-25 20:33:19 -0600

    edit

--- script.js
+++ script.js
@@ -11,17 +11,42 @@ const divideBtn = document.getElementById('btn/');
 const equalBtn = document.getElementById('btn=');
 const clearBtn = document.getElementById('btnC');
 
-let arg1 = 0;
-let arg2 = 0;
-let result = 0;
+let arg1 = ['21+*'];
+let arg2 = [];
+let result = [];
 
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-            arg1 += `${button.dataset.number}`;
+class Stack {
+    constructor()
+    {
+        this.items = [];
+    }
+    push(element) 
+    {
+        this.items.push(element);
+    }
+    pop()
+    {
+        if (this.items.length == 0) 
+            return 'Underflow';
+        return this.items.pop();
+    }
+    peek() 
+    {
+        return this.items[this.items.length - 1];
+    }
+    isEmpty()
+    {
+        return this.items.length = 0;
+    }
+    printStack()
+    {
+        let str = "";
+        for (var i = 0; i < this.items.length; i++)
+            str += this.items[i] + " ";
+        return str;
+    }
+}
 
-        
-    })
-})
 
 plusBtn.addEventListener('click', () => {
     displayResult.textContent = '+';
@@ -46,25 +71,46 @@ clearBtn.addEventListener('click', () => {
 equalBtn.addEventListener('click', () => {
 })
 
-class Stack {
-    constructor()
-    {
-        this.items = [];
-    }
-    push(arg1) 
-    {
-        this.items.push(arg1)
-    }
-    isEmpty()
-    {
-        return this.items.length = 0;
-    }
-    printStack()
-    {
-        var str = "";
-        for (var i = 0; i < this.items.length; i++)
-            str += this.items[i] + " ";
-        return str;
-    }
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        arg1.push(button.dataset.number);
+    })
+})
 
+
+function postFixEvaluation(exp)
+{
+    var stack = new Stack();
+    for (var i = 0; i < exp.length; i++) {
+        var c = exp[i];
+        if (!isNaN(c))
+            stack.push(c - '0');
+        else {
+            var val1 = stack.pop();
+            var val2 = stack.pop();
+            if (val1 == "Underflow" || val2 == "Underflow")
+                return "Can't perform postfix evaluation";
+            switch (c) {
+            case '+':
+                stack.push(val2 + val1);
+                break;
+  
+            case '-':
+                stack.push(val2 - val1);
+                break;
+  
+            case '/':
+                stack.push(val2 / val1);
+                break;
+  
+            case '*':
+                stack.push(val2 * val1);
+                break;
+            }
+        }
+    }
+  
+    return stack.pop();
 }
+
+
