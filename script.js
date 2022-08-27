8b2307dfa2a588c0a595e3baef3a9ce0fb3cef93
Sava <savab2164@gmail.com>
2022-08-26 21:59:07 -0600

    Redid the whole code, and logic. Need to figure out how to do multiple inputs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const body = document.body
 const container = document.querySelector('.container');
 const displayResult = document.querySelector('#result');
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('[data-number]');
 
 // operation buttons
 const plusBtn = document.getElementById('btn+');
@@ -11,10 +11,23 @@ const divideBtn = document.getElementById('btn/');
 const equalBtn = document.getElementById('btn=');
 const clearBtn = document.getElementById('btnC');
 
+const add = (a,b) => { return parseInt(a) + parseInt(b) };
+const subtract = (a,b) => { return parseInt(a) - parseInt(b) };
+const multiply = (a,b) => { return parseInt(a) * parseInt(b) };
+const divide = (a,b) => { return parseInt(a) / parseInt(b) };
 
-let arg1 = 0;
-let arg2 = 0;
-let result = 0;
+const newOp = () => {
+    if (displayValue > 0) {
+        arg1 = displayValue;
+        arg2 = 0;
+        doneFirstArg = 1;
+        displayValue = 0;
+    }
+}
+
+let arg1 = '';
+let arg2 = '';
+let displayValue = 0;
 let doneFirstArg = 0;
 let operation = 0;
 
@@ -22,64 +35,71 @@ let operation = 0;
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
         if (doneFirstArg == 0) {
-            arg1 += button.dataset.number;
+            arg1 += parseInt(`${button.dataset.number}`);
             displayResult.textContent = arg1;
+            console.log(arg1, arg2, result)
 
         } else {
-            arg2 += button.dataset.number;
+            arg2 += parseInt(`${button.dataset.number}`);
             displayResult.textContent = arg2;
+            console.log(arg1, arg2, result)
 
         }
     })
 })
 
-const checkArg = () => { doneFirstArg = doneFirstArg == 0 ? 1: 0 };
-
 plusBtn.addEventListener('click', () => {
-    checkArg()
+    newOp();
     operation = 1;
+    doneFirstArg ++;
     displayResult.textContent = '+';
-
+    console.log(arg1, arg2, result)
 });
 minusBtn.addEventListener('click', () => {
-    checkArg()
+    newOp();
     operation = 2;
+    doneFirstArg ++;
     displayResult.textContent = '-';
-
+    console.log(arg1, arg2, result)
 });
 multipBtn.addEventListener('click', () => {
-    checkArg()
+    newOp();
     operation = 3;
+    doneFirstArg ++;
     displayResult.textContent = 'x';
-
+    console.log(arg1, arg2, result);
 });
 divideBtn.addEventListener('click', () => {
-    checkArg()
+    newOp();
     operation = 4;
+    doneFirstArg ++;
     displayResult.textContent = '÷';
-
+    console.log(arg1, arg2, result);
 });
 
 clearBtn.addEventListener('click', () => {
-    arg1 = 0;
-    arg2 = 0;
-    result = 0;
-    doneFirstArg = 0;
-    operation = 0;
+    displayResult.textContent = 0;
+    console.log(arg1, arg2, result);
 })
+
+const calculateNumbers = () => {
+    if (operation == 1) {
+        displayValue += add(arg1, arg2);
+    } else if (operation == 2){
+        displayValue += subtract(arg1, arg2);
+    } else if (operation == 3){
+        displayValue += multiply(arg1, arg2);
+    } else if (operation == 4) {
+        displayValue += divide(arg1, arg2);
+    }
+}
 
 // press equal and get result
 equalBtn.addEventListener('click', () => {
-    if (operation == 1) {
-        result = arg1 + arg2;
-    } else if (operation == 2) {
-        result = arg1 - arg2;
-    } else if (operation == 3) {
-        result = arg1 * arg2;
-    } else if (operation == 4) {
-        result = arg1 / arg2;
-    }
-    displayResult.textContent = parseInt(result);
+    newOp();
+    calculateNumbers();
+    displayResult.textContent = displayValue;
+    console.log(arg1, arg2, result);
 })
 
    
